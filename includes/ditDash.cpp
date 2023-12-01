95241b6d7225fd44bbbbde087724f1f93e80042b
Harsh Mawandia <61311217+DoubleBerserker@users.noreply.github.com>
2023-12-02 00:41:41 +0530

    Updated the definitions of operations on WAV file.

--- includes/ditDash.cpp
+++ includes/ditDash.cpp
@@ -1,5 +1,41 @@
-//
-// Created by harsh on 02-12-2023.
-//
-
 #include "ditDash.h"
+
+void dit(std::ofstream &file, Oscillator oscillator) {
+    oscillator.reset();
+    for (int i = 0; i < sampleRate * unitDuration; i++) {
+        auto sample = oscillator.process();
+        int intSample = static_cast<int>(sample * maxAmplitude);
+        file.write(reinterpret_cast<char *>(&intSample), 2);
+    }
+    for(int i = 0; i < sampleRate * unitDuration; i++){
+        int intSample = 0;
+        file.write(reinterpret_cast<char *>(&intSample), 2);
+    }
+}
+
+void dash(std::ofstream &file, Oscillator oscillator) {
+    oscillator.reset();
+    for (int i = 0; i < sampleRate * unitDuration * 3; i++) {
+        auto sample = oscillator.process();
+        int intSample = static_cast<int>(sample * maxAmplitude);
+        file.write(reinterpret_cast<char *>(&intSample), 2);
+    }
+    for(int i = 0; i < sampleRate * unitDuration; i++){
+        int intSample = 0;
+        file.write(reinterpret_cast<char *>(&intSample), 2);
+    }
+}
+
+void charSpace(std::ofstream &file, Oscillator oscillator){
+    for(int i = 0; i < sampleRate * unitDuration * 3; i++){
+        int intSample = 0;
+        file.write(reinterpret_cast<char *>(&intSample), 2);
+    }
+}
+
+void wordSpace(std::ofstream &file, Oscillator oscillator){
+    for(int i = 0; i < sampleRate * unitDuration * 3; i++){
+        int intSample = 0;
+        file.write(reinterpret_cast<char *>(&intSample), 2);
+    }
+}
--- includes/ditDash.h
+++ includes/ditDash.h
@@ -1,8 +1,13 @@
-//
-// Created by harsh on 02-12-2023.
-//
-
 #ifndef MORSIN_TIME_DITDASH_H
 #define MORSIN_TIME_DITDASH_H
+
+#include <fstream>
+#include "Oscillator.h"
+#include "consts.h"
+
+void dit(std::ofstream &file, Oscillator oscillator);
+void dash(std::ofstream &file, Oscillator oscillator);
+void charSpace(std::ofstream &file, Oscillator oscillator);
+void wordSpace(std::ofstream &file, Oscillator oscillator);
 
 #endif //MORSIN_TIME_DITDASH_H
