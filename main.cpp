4adae461d1dd4e09908f1c5c181a33adc69ba9ed
Harsh Mawandia <61311217+DoubleBerserker@users.noreply.github.com>
2023-12-01 20:29:19 +0530

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,144 @@
+//#include <iostream>
+//#include <cmath>
+//#include <fstream>
+//
+//const int sampleRate = 44100;
+//const int bitDepth = 16;
+//
+//class Oscillator{
+//    float frequency, amplitude, angle = 0.0f, offset;
+//public:
+//    Oscillator(float freq, float amp) : frequency(freq), amplitude(amp) {
+//        offset = 2 * M_PI * frequency / sampleRate;
+//    }
+//    float process() {
+//        auto sample = amplitude * sin(angle);
+//        angle += offset;
+//        return sample;
+//    }
+//};
+//
+//int main(){
+//    int duration = 2;
+//    std::ofstream audioFile;
+//    audioFile.open("waveform", std::ios::binary);
+//    Oscillator oscillator(440, 0.5);
+//
+//    auto maxAmplitude = pow(2, bitDepth - 1) - 1;
+//    for (int i = 0; i < sampleRate * duration; ++i) {
+//        auto sample = oscillator.process();
+//        int intSample = static_cast<int> (sample * maxAmplitude);
+//        audioFile.write(reinterpret_cast<char *> (&intSample), 2);
+//    }
+//    audioFile.close();
+//    return 0;
+//}
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+const int sampleRate = 44100;
+const int bitDepth = 16;
+//const float ditDuration = 0.3;
+//const float dashDuration = 1.2;
+//const float silenceDuration = 0.3;
+const float unitDuration = 0.1;
+const auto maxAmplitude = pow(2, bitDepth - 1) - 1;
+
+class Oscillator{
+    float frequency, amplitude, angle = 0.0f, offset;
+public:
+    Oscillator(float freq, float amp) : frequency(freq), amplitude(amp) {
+        offset = 2 * M_PI * frequency / sampleRate;
+    }
+
+    float process() {
+        auto sample = amplitude * sin(angle);
+        angle += offset;
+        return sample;
+    }
+
+    void reset(){
+        angle = 0.0f;
+    }
+};
+
+void dit(std::ofstream &file, Oscillator oscillator);
+void dash(std::ofstream &file, Oscillator oscillator);
+void charSpace(std::ofstream &file, Oscillator oscillator);
+void wordSpace(std::ofstream &file, Oscillator oscillator);
+
+int main(){
+    std::ofstream audioFile;
+    audioFile.open("waveform", std::ios::binary);
+    Oscillator oscillator(440, 0.5);
+//    auto maxAmplitude = pow(2, bitDepth - 1) - 1;
+
+    // S - dit dit dit
+    dit(audioFile, oscillator);
+    dit(audioFile, oscillator);
+    dit(audioFile, oscillator);
+
+    charSpace(audioFile, oscillator);
+
+    dash(audioFile, oscillator);
+    dash(audioFile, oscillator);
+    dash(audioFile, oscillator);
+
+    charSpace(audioFile, oscillator);
+
+    dit(audioFile, oscillator);
+    dit(audioFile, oscillator);
+    dit(audioFile, oscillator);
+
+    audioFile.close();
+    return 0;
+}
+
+void dit(std::ofstream &file, Oscillator oscillator){
+    oscillator.reset();
+    for(int i = 0; i < sampleRate * unitDuration; i++){
+        auto sample = oscillator.process();
+        int intSample = static_cast<int>(sample * maxAmplitude);
+        file.write(reinterpret_cast<char *>(&intSample), 2);
+    }
+
+    for(int i = 0; i < sampleRate * unitDuration; i++){
+        int intSample = 0;
+        file.write(reinterpret_cast<char *>(&intSample), 2);
+    }
+
+}
+
+void dash(std::ofstream &file, Oscillator oscillator){
+//    file.open("waveform", std::ios::binary);
+    oscillator.reset();
+    for(int i = 0; i < sampleRate * unitDuration * 3; i++){
+        auto sample = oscillator.process();
+        int intSample = static_cast<int>(sample * maxAmplitude);
+        file.write(reinterpret_cast<char *>(&intSample), 2);
+    }
+
+    for(int i = 0; i < sampleRate * unitDuration; i++){
+        int intSample = 0;
+        file.write(reinterpret_cast<char *>(&intSample), 2);
+    }
+
+}
+
+void charSpace(std::ofstream &file, Oscillator oscillator){
+//    file.open("waveform", std::ios::binary);
+    for(int i = 0; i < sampleRate * unitDuration * 3; i++){
+        int intSample = 0;
+        file.write(reinterpret_cast<char *>(&intSample), 2);
+    }
+}
+
+void wordSpace(std::ofstream &file, Oscillator oscillator){
+//    file.open("waveform", std::ios::binary);
+    for(int i = 0; i < sampleRate * unitDuration * 3; i++){
+        int intSample = 0;
+        file.write(reinterpret_cast<char *>(&intSample), 2);
+    }
+}
